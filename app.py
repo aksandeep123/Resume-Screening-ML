2446d9db1d746b4ed3af42c4faf44dddc722b4a3
Sandeep Kumar <845sandeepvishwakarma11scif@gmail.com>
2024-10-14 18:55:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import pickle
 app = Flask(__name__)
 
 # Load models===========================================================================================================
-rf_classifier_categorization = pickle.load(open('models/rf_classifier_categorization.pkl', 'rb'))
-tfidf_vectorizer_categorization = pickle.load(open('models/tfidf_vectorizer_categorization.pkl', 'rb'))
-rf_classifier_job_recommendation = pickle.load(open('models/rf_classifier_job_recommendation.pkl', 'rb'))
-tfidf_vectorizer_job_recommendation = pickle.load(open('models/tfidf_vectorizer_job_recommendation.pkl', 'rb'))
+rf_classifier_categorization = pickle.load(open('C:/Users/Admin/Desktop/projects1/Resume-Screening-with-Machine-Learning-Job-Recommendations-Parsing-Categorization/models/rf_classifier_categorization.pkl', 'rb'))
+tfidf_vectorizer_categorization = pickle.load(open('C:/Users/Admin/Desktop/projects1/Resume-Screening-with-Machine-Learning-Job-Recommendations-Parsing-Categorization/models/tfidf_vectorizer_categorization.pkl', 'rb'))
+rf_classifier_job_recommendation = pickle.load(open('C:/Users/Admin/Desktop/projects1/Resume-Screening-with-Machine-Learning-Job-Recommendations-Parsing-Categorization/models/rf_classifier_job_recommendation.pkl', 'rb'))
+tfidf_vectorizer_job_recommendation = pickle.load(open('C:/Users/Admin/Desktop/projects1/Resume-Screening-with-Machine-Learning-Job-Recommendations-Parsing-Categorization/models/tfidf_vectorizer_job_recommendation.pkl', 'rb'))
 
 # Clean resume==========================================================================================================
 def cleanResume(txt):
